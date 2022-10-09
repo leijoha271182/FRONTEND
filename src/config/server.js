9f6e849a341d92afe6eecc216a0a2482aeb873d1
Cesar Zuluaga <cesar.zuluaga@aliatic.com.co>
2022-10-09 13:26:33 -0500

    add remote server

--- src/config/server.js
+++ src/config/server.js
@@ -1,5 +1,6 @@
 const serverConfig = {
-    urlBaseServer:"http://localhost:4000"
+    // urlBaseServer:"http://localhost:4000"
+    urlBaseServer:"https://inventarios-api-universidad.herokuapp.com"
     } 
 
 export default serverConfig
